5a46f8ba05a8243ca74f4cda0769e3c22c194bc1
Guilherme Cunha <1201506@isep.ipp.pt>
2024-11-05 09:52:59 +0000

    Smoke Test update

--- psoft-project-2024-g1/src/test/java/pt/psoft/g1/psoftg1/SmokeTeste.java
+++ psoft-project-2024-g1/src/test/java/pt/psoft/g1/psoftg1/SmokeTeste.java
@@ -22,6 +22,7 @@ import org.springframework.web.client.RestTemplate;
 import pt.psoft.g1.psoftg1.authormanagement.api.AuthorView;
 
 import org.springframework.http.*;
+import pt.psoft.g1.psoftg1.usermanagement.api.UserView;
 
 
 import java.security.cert.X509Certificate;
@@ -38,14 +39,16 @@ public class SmokeTeste {
     private RestTemplateBuilder restTemplateBuilder;
 
     private RestTemplate restTemplate;
+    private UserView userView;
 
     private final String BASE_URL = "https://vs-gate.dei.isep.ipp.pt:31362/api/";
+    private String authToken;
 
     @BeforeEach
     public void setUp() throws Exception {
         // Configuração do SSL
         SSLContext sslContext = SSLContextBuilder.create()
-                .loadTrustMaterial((X509Certificate[] certificateChain, String authType) -> true) // certificados
+                .loadTrustMaterial((X509Certificate[] certificateChain, String authType) -> true)
                 .build();
 
         Registry<ConnectionSocketFactory> socketRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
@@ -58,33 +61,56 @@ public class SmokeTeste {
                 .setConnectionManagerShared(true)
                 .build();
 
-        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        // inicialização do RestTemplate
+        new HttpComponentsClientHttpRequestFactory(httpClient);
         this.restTemplate = restTemplateBuilder
                 .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(httpClient))
                 .build();
-    }
 
+        String url = BASE_URL + "public/login";
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String loginRequestJson = """
+                {
+                    "username": "maria@gmail.com",
+                    "password": "Mariaroberta!123"
+                }
+                """;
+
+        HttpEntity<String> entity = new HttpEntity<>(loginRequestJson, headers);
+
+        // Act
+        ResponseEntity<UserView> response = restTemplate.exchange(url, HttpMethod.POST, entity, UserView.class);
+        userView = response.getBody();
+
+        this.authToken = response.getHeaders().getFirst("Authorization");
+
+        // Assertions
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(userView).isNotNull();
+    }
 
     @Test
     public void testFindByAuthorNumber_Success() {
-        // Arrange
-        String authorNumber = "1";
-        String url = BASE_URL + "authors" + "/" + authorNumber;
 
-        // cabeçalhos
+        assertThat(this.authToken).isNotNull();
+
+        String authorNumber = "1";
+        String url = BASE_URL + "authors/" + authorNumber;
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Bearer " + "eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJleGFtcGxlLmlvIiwic3ViIjoiMyxwZWRyb0BnbWFpbC5jb20iLCJleHAiOjE3MzA3OTQ2ODQsImlhdCI6MTczMDc1ODY4NCwicm9sZXMiOiJSRUFERVIifQ.RjiWWHPpA7blOVW-2pIPstTi8lFni_HQZBQomUYbmHQkqNueHR6mvkk9pUbJ9FPy1NXyY3ow13cojrfBt_eC7u8McDdE2JyBXpEGAu4wCc2uXJM2FK4fkj5okjly_pOIT7yx3prllOojU9_XzRtUk_oCCoREOPMPB68f5FjJzpDiL9DILvEUaHo02M_ZqcLl61UMni-rXSerBPonoQt11jiqMmXRMx3ecQrnTxj2mcnNV4IJqlycNX4gEwyYzYk5DXm_qsJvB2NDzXvFswMn_Gy9aPk0QT4qX-35a5WZAIi6frFPB_UYqoNJe3zAI8LZ5QHIaArl7mWbGF6QtPCh9k_xPOYjBYVxTT-PnkEa47KqVKNGHzWOiII_Wkk2d_fc3OrN-zKB_J2g73TAFOPWr_09WCPXeYahJ9nG1-pxp0qldTuIDzrkUHQLJEK4f7OayAO14YMrXKe9lMs8NoPxOdj9-KBa-2VvlTrqP0LEknSfkd165cMBTYUPdTnuRYCwHAe6YHW45fJ67ooe4sEvn9-g8wHP50KwuTGxswfvze0Cmw0uYtjFvuDspeC5vThBsNS_nsYo8JELrESRGhxbSsRgz_nbCWli_nlhCr5AYzvUQklxqckrzzyLZmekb9g_vyRDBHV8p2nwoOap01WTNodrE8_4o2qQNh7VTe7K9og");
+        headers.set("Authorization", "Bearer " + this.authToken);
 
-        // entidade da requisiçao
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         // Act
         ResponseEntity<AuthorView> response = restTemplate.exchange(url, HttpMethod.GET, entity, AuthorView.class);
         AuthorView authorView = response.getBody();
 
-        // Assertions
+        // Assert
         assertThat(authorView).isNotNull();
         assertThat(authorView.getName()).isEqualTo("Manuel Antonio Pina");
     }
