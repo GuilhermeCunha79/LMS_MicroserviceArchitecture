7a1a96cce161d7e80fa119e967b395310765a851
Guilherme Cunha <1201506@isep.ipp.pt>
2025-01-03 21:52:40 -0100

    Performance test per user container pipeline modification

--- psoft-project-2024-g1/AuthNUserCommand/init-scripts/init_db.sh
+++ psoft-project-2024-g1/AuthNUserCommand/init-scripts/init_db.sh
@@ -1,15 +1,3 @@
-#!/bin/bash
-DB_NAME="${POSTGRES_DB:-test_users_$(taskslot)}"  # Expansão correta de hostname
-
-# Exibir o nome do banco de dados que será criado
-echo "Tentando criar o banco de dados: $DB_NAME"
-
-# Verificar se o banco de dados já existe
-DB_EXISTS=$(psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='${DB_NAME}'")
-
-if [ "$DB_EXISTS" != "1" ]; then
-  echo "Criando o banco de dados: $DB_NAME"
-  psql -U postgres -c "CREATE DATABASE \"$DB_NAME\";"
-else
-  echo "O banco de dados $DB_NAME já existe."
-fi
+CREATE DATABASE test_users_1;
+CREATE DATABASE test_users_2;
+CREATE DATABASE test_users_3;
